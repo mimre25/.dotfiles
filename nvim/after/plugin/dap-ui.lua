efdfd716729825b1338bd088d266ce822c0c9feb
Martin Imre <martinimre25@gmail.com>
2024-01-22 16:15:58 +0100

    feat(debugging): Auto-open dap-ui when starting debugging session

--- nvim/after/plugin/dap-ui.lua
+++ nvim/after/plugin/dap-ui.lua
@@ -3,4 +3,20 @@ local dapui = require("dapui")
 
 dapui.setup()
 
-vim.keymap.set({"n","v"}, "<leader>du", dapui.toggle)
+vim.keymap.set({"n","v"}, "<leader>dui", dapui.toggle)
+
+
+local dap, dapui = require("dap"), require("dapui")
+dap.listeners.before.attach.dapui_config = function()
+  dapui.open()
+end
+dap.listeners.before.launch.dapui_config = function()
+  dapui.open()
+end
+dap.listeners.before.event_terminated.dapui_config = function()
+  dapui.close()
+end
+dap.listeners.before.event_exited.dapui_config = function()
+  dapui.close()
+end
+
