578908f0634bc994bf84053c89cab36344391488
Martin Imre <martinimre25@gmail.com>
2024-01-23 20:28:47 +0100

    conf(dap-ui): Don't close dap-ui when debug run is over

--- nvim/after/plugin/dap-ui.lua
+++ nvim/after/plugin/dap-ui.lua
@@ -13,10 +13,10 @@ end
 dap.listeners.before.launch.dapui_config = function()
   dapui.open()
 end
-dap.listeners.before.event_terminated.dapui_config = function()
-  dapui.close()
-end
-dap.listeners.before.event_exited.dapui_config = function()
-  dapui.close()
-end
+--dap.listeners.before.event_terminated.dapui_config = function()
+--  dapui.close()
+--end
+--dap.listeners.before.event_exited.dapui_config = function()
+--  dapui.close()
+--end
 
