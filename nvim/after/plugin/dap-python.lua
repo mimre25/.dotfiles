985fb1b20bdc09b8e6512c82f93342c18768b4dd
Martin Imre <martin.imre@raicoon.com>
2024-01-23 13:10:03 +0100

    conf(dap): Configure python interpreter to use micromamba

--- nvim/after/plugin/dap-python.lua
+++ nvim/after/plugin/dap-python.lua
@@ -1,4 +1,4 @@
 local dap_python = require('dap-python')
-dap_python.setup('/home/martin/miniconda3/bin/python')
+dap_python.setup('/home/martin/micromamba/bin/python')
 
 dap_python.test_runner = "pytest"
