5da2cb4705c518beec50d69955f9808eb1b96296
Martin Imre <martinimre25@gmail.com>
2024-01-28 16:30:55 +0100

    fix(neovim): Fix keymappings for going back (gb vs b)
    Turns out normal motions are not listed in the keymap (:map) and I
    forgot about "b" being backwards word-wise jumps

--- nvim/after/plugin/comment.nvim.lua
+++ nvim/after/plugin/comment.nvim.lua
@@ -1,1 +1,16 @@
-require("Comment").setup()
+require("Comment").setup({
+    toggler = {
+        ---Line-comment toggle keymap
+        line = 'gcc',
+        ---Block-comment toggle keymap
+        block = 'cbc',
+    },
+    ---LHS of operator-pending mappings in NORMAL and VISUAL mode
+    opleader = {
+        ---Line-comment keymap
+        line = 'gc',
+        ---Block-comment keymap
+        block = 'cb',
+    },
+})
+
