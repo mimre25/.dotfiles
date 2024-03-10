5b0116f79c062d71b6122d24eb641fc02ed746fd
Martin Imre <martinimre25@gmail.com>
2024-03-10 17:58:40 +0100

    feat(nvim): Only highlight sql syntax in triple-quote python strings

--- nvim/queries/python/injections.scm
+++ nvim/queries/python/injections.scm
@@ -2,5 +2,8 @@
 ; ((string) @sql (#contains? @sql "SELECT" "INSERT" "UPDATE" "DELETE" "CREATE" "ALTER") (#offset! @sql 1 0 -1 0))
 
 (string
+  (string_start) @start
   (string_content) @injection.content
+    (#match? @start "^[\"']{3}")
     (#set! injection.language "sql"))
+
