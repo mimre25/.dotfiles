dad214564f786f1a9444c302d913fdc3bf0131ee
Martin Imre <martinimre25@gmail.com>
2024-04-21 17:00:10 +0200

    feat(nvim): Add keymapping for jsonfly
    This further adds a function allowing jsonfly to work on unnamed
    buffers.

---
+++ nvim/after/plugin/jsonfly.nvim.lua
@@ -0,0 +1,13 @@
+local lsp = require('lsp-zero')
+
+vim.keymap.set("n", "<leader>j", function()
+    if vim.bo.filetype == '' then
+        vim.bo.filetype = 'json'
+        lsp.new_client({
+            filetypes = 'json',
+            on_attach = vim.cmd('Telescope jsonfly')
+        })
+    else
+        vim.cmd('Telescope jsonfly')
+    end
+end)
