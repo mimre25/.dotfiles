9349d32601ffcf2366a06e3dc42e1332c5b52ccf
Martin Imre <martinimre25@gmail.com>
2024-06-22 12:16:20 +0200

    feat(nvim): Add cheatsheet to search commands and such

--- nvim/lua/mimre/lazy.lua
+++ nvim/lua/mimre/lazy.lua
@@ -279,4 +279,41 @@ require("lazy").setup({
 			require("dbee").setup( --[[optional config]])
 		end,
 	},
+	-- cheatsheet for vim commands
+	{
+		"doctorfree/cheatsheet.nvim",
+		event = "VeryLazy",
+		dependencies = {
+			{ "nvim-telescope/telescope.nvim" },
+			{ "nvim-lua/popup.nvim" },
+			{ "nvim-lua/plenary.nvim" },
+		},
+		config = function()
+			local ctactions = require("cheatsheet.telescope.actions")
+			require("cheatsheet").setup({
+				bundled_cheatsheets = {
+					enabled = { "default", "lua", "markdown", "regex", "netrw", "unicode" },
+					disabled = { "nerd-fonts" },
+				},
+				bundled_plugin_cheatsheets = {
+					enabled = {
+						"auto-session",
+						"goto-preview",
+						"octo.nvim",
+						"telescope.nvim",
+						"vim-easy-align",
+						"vim-sandwich",
+					},
+					disabled = { "gitsigns" },
+				},
+				include_only_installed_plugins = true,
+				telescope_mappings = {
+					["<CR>"] = ctactions.select_or_fill_commandline,
+					["<A-CR>"] = ctactions.select_or_execute,
+					["<C-Y>"] = ctactions.copy_cheat_value,
+					["<C-E>"] = ctactions.edit_user_cheatsheet,
+				},
+			})
+		end,
+	},
 })
