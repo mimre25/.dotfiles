1792c830bcbdddd21ed6436b5a1b950a28046493
Martin Imre <martinimre25@gmail.com>
2024-07-01 09:29:56 +0200

    feat(nvim): Switch db to dadbod instead of dbee (alpha)

--- nvim/lua/mimre/lazy.lua
+++ nvim/lua/mimre/lazy.lua
@@ -291,22 +291,22 @@ require("lazy").setup({
 			})
 		end,
 	},
-    -- databases
-    {
-      "kristijanhusak/vim-dadbod-ui",
-      dependencies = {
-        { "tpope/vim-dadbod", lazy = true },
-        { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
-      },
-      cmd = {
-        "DBUI",
-        "DBUIToggle",
-        "DBUIAddConnection",
-        "DBUIFindBuffer",
-      },
-      init = function()
-        -- Your DBUI configuration
-        vim.g.db_ui_use_nerd_fonts = 1
-      end,
-    }
+	-- databases
+	{
+		"kristijanhusak/vim-dadbod-ui",
+		dependencies = {
+			{ "tpope/vim-dadbod", lazy = true },
+			{ "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+		},
+		cmd = {
+			"DBUI",
+			"DBUIToggle",
+			"DBUIAddConnection",
+			"DBUIFindBuffer",
+		},
+		init = function()
+			-- Your DBUI configuration
+			vim.g.db_ui_use_nerd_fonts = 1
+		end,
+	},
 })
